8893c4346cb56772c63ee669407134dd8156106e
Gert <gert.kanter@gmail.com>
2013-01-30 20:11:50 +0200

    v0.1.0  - algorithm solves IK problems without constraints

--- cFABRIK.cpp
+++ cFABRIK.cpp
@@ -47,9 +47,11 @@ namespace cFABRIK
 	point cFABRIK::makept(double x, double y, double z)
 	{
 		point *retval = new point;
+
 		retval->x = x;
 		retval->y = y;
 		retval->z = z;
+
 		return(*retval);
 	}
 
@@ -74,12 +76,36 @@ namespace cFABRIK
 		return((double) (sqrt((pow(pt2.x-pt1.x, 2) + pow(pt2.y-pt1.y, 2) + pow(pt2.z-pt1.z, 2)))));
 	}
 
-	int cFABRIK::iterate(point target)
+	point cFABRIK::ptscale(point pt, double scale)
+	{
+		point *retval = new point;
+
+		retval->x = pt.x * scale;
+		retval->y = pt.y * scale;
+		retval->z = pt.z * scale;
+
+		return(*retval);
+	}
+
+	point cFABRIK::ptadd(point pt1, point pt2)
+	{
+		point *retval = new point;
+
+		retval->x = pt1.x + pt2.x;
+		retval->y = pt1.y + pt2.y;
+		retval->z = pt1.z + pt2.z;
+
+		return(*retval);
+	}
+
+	int cFABRIK::solve(point target)
 	{
 		double* d = new double[cFABRIK::size-1];
 		double totaldist = 0;
 
-		for(int i=0;i<cFABRIK::size-1;i++)
+		int count = 0;
+
+		for(int i=0;i<=cFABRIK::size-2;i++)
 		{
 			d[i] = cFABRIK::ptdifflen(cFABRIK::pts[i+1], cFABRIK::pts[i]);
 			totaldist += d[i];
@@ -88,29 +114,72 @@ namespace cFABRIK
 		if(cFABRIK::ptdifflen(cFABRIK::pts[0], target) > totaldist)
 		{
 			// target is unreachable
-			double* dist = new double[cFABRIK::size-1];
+			double* r = new double[cFABRIK::size-1];
 			double* l = new double[cFABRIK::size-1];
 
-			for(int i=0;i<cFABRIK::size-1;i++)
+			for(int i=0;i<=cFABRIK::size-2;i++)
 			{
 				// find the distance between the target and the joint position
-				dist[i] = cFABRIK::ptdifflen(target, cFABRIK::pts[i]);
-				l[i]  = d[i] / dist[i];
+				r[i] = cFABRIK::ptdifflen(target, cFABRIK::pts[i]);
+				l[i]  = d[i] / r[i];
 				// find the new joint positions
-				//cFABRIK::pts[i+1] = (1 - l[i])
+				cFABRIK::pts[i+1] = cFABRIK::ptadd(cFABRIK::ptscale(cFABRIK::pts[i], (1 - l[i])), cFABRIK::ptscale(target, l[i]));
 			}
 
-			delete [] dist;
+			delete [] r;
 			delete [] l;
 			delete [] d;
-			return(0);
+			return(-1);
 		}
 		else
 		{
 			// target is reachable; thus, set b as the initial position of the joint pts[0]
+			point *b = new point;
 
+			double* r = new double[cFABRIK::size-1];
+			double* l = new double[cFABRIK::size-1];
+			int i = 0;
+
+			*b = cFABRIK::pts[0];
+			// check whether the distance between the end effector pts[size-1] and the target is greater than a tolerance
+			double EEdiff = cFABRIK::ptdifflen(cFABRIK::pts[cFABRIK::size-1], target);
+
+			while(EEdiff > 0.001)
+			{
+				count++;
+				// STAGE 1: FORWARD REACHING
+				// Set the end effector pts[size-1] as target t
+				cFABRIK::pts[cFABRIK::size-1] = target;
+
+				for(i=cFABRIK::size-2;i>=0;i--)
+				{
+					// find the distance r_i between the new joint position pts_i+1 and the joints pts_i
+					r[i] = cFABRIK::ptdifflen(cFABRIK::pts[i+1], cFABRIK::pts[i]);
+					l[i]  = d[i] / r[i];
+					// find the new joint positions pts_i
+					cFABRIK::pts[i] = cFABRIK::ptadd(cFABRIK::ptscale(cFABRIK::pts[i+1], (1 - l[i])), cFABRIK::ptscale(cFABRIK::pts[i], l[i]));
+				}
+				// STAGE 2: BACKWARD REACHING
+				// set the root pts[0] its initial position
+				cFABRIK::pts[0] = *b;
+
+				for(i=0;i<=cFABRIK::size-2;i++)
+				{
+					// find the distance r_i between the new joint position pts_i and the joint pts_i+1
+					r[i] = cFABRIK::ptdifflen(cFABRIK::pts[i+1], cFABRIK::pts[i]);
+					l[i]  = d[i] / r[i];
+					// find the new joint positions pts_i
+					cFABRIK::pts[i+1] = cFABRIK::ptadd(cFABRIK::ptscale(cFABRIK::pts[i], (1 - l[i])), cFABRIK::ptscale(cFABRIK::pts[i+1], l[i]));
+				}
+
+				EEdiff = cFABRIK::ptdifflen(cFABRIK::pts[cFABRIK::size-1], target);
+			}
+
+
+			delete [] r;
+			delete [] l;
 			delete [] d;
-			return(0);
+			return(count);
 		}
 
 
--- cFABRIK.h
+++ cFABRIK.h
@@ -26,8 +26,10 @@ namespace cFABRIK
 		__declspec(dllexport) point __stdcall makept(double x, double y, double z);
 		__declspec(dllexport) int __stdcall setpts(point points[], int size);
 		__declspec(dllexport) int __stdcall getpts(point** points);
-		__declspec(dllexport) int __stdcall iterate(point target);
+		__declspec(dllexport) int __stdcall solve(point target);
 		__declspec(dllexport) double __stdcall ptdifflen(point pt1, point pt2);
+		__declspec(dllexport) point __stdcall cFABRIK::ptscale(point pt, double scale);
+		__declspec(dllexport) point __stdcall cFABRIK::ptadd(point pt1, point pt2);
 	private:
 		point* pts;
 		int size;
